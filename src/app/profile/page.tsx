cdc3aaf116ebe153b5d380155f722342eb33ea06
Favour Aniogor <faniogor@gmail.com>
2023-10-16 01:09:33 +0100

    This will add Not Found page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -9,6 +9,7 @@ import { Clipboard, DirArrow, GreyVerified, SearchIcon } from "@/utils/icons";
 import {
   Button,
   HStack,
+  Heading,
   Input,
   InputGroup,
   Modal,
@@ -48,8 +49,6 @@ function Profile() {
           acc?.volume + curr?.volume;
         }, 0)
       );
-    } else {
-      router.push("/");
     }
   }, [isAuth]);
 
@@ -122,79 +121,99 @@ function Profile() {
     setMBody(Alement);
   }
 
-  return (
-    <main>
-      {/* {Header} */}
-      <Header />
-      {/* {Main Content} */}
+  if (!isAuth) {
+    return (
       <Stack
-        padding={["0.98rem 0.88rem", "0.98rem 0.88rem", "1.88rem 4.63rem"]}
+        w={"100%"}
+        height={"100vh"}
+        alignItems={"center"}
+        justifyContent={"center"}
       >
-        {/* {Modal} */}
-        <Modal isCentered isOpen={isOpen} onClose={onClose}>
-          <Overlay />
-          <ModalContent>
-            <ModalHeader
-              color={"#000000"}
-              fontSize={["1.5rem", "1.5rem", "2.25rem"]}
-              fontFamily={"NexaBold"}
-            >
-              {mHeading}
-            </ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>{mBody}</ModalBody>
-            <ModalFooter>
-              <Button
-                onClick={() => {
-                  onClose();
-                  setMBody(<CreateOptions />);
-                  setMHeading("Create");
-                }}
-              >
-                Close
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-        {/* {Image Header} */}
-        <ImageHeader header={profHeader} avatar={profAvatar} />
-        <HStack
+        <Heading
+          fontSize={["1.5rem", "1.5rem", "2.25rem"]}
           fontFamily={"NexaBold"}
-          paddingTop={"4.5rem"}
-          alignItems={"center"}
-          gap={["1.5rem", "1.5rem", "3.5rem"]}
         >
+          404 NOT FOUND
+        </Heading>
+      </Stack>
+    );
+  } else {
+    return (
+      <main>
+        {/* {Header} */}
+        <Header />
+        {/* {Main Content} */}
+        <Stack
+          padding={["0.98rem 0.88rem", "0.98rem 0.88rem", "1.88rem 4.63rem"]}
+        >
+          {/* {Modal} */}
+          <Modal isCentered isOpen={isOpen} onClose={onClose}>
+            <Overlay />
+            <ModalContent>
+              <ModalHeader
+                color={"#000000"}
+                fontSize={["1.5rem", "1.5rem", "2.25rem"]}
+                fontFamily={"NexaBold"}
+              >
+                {mHeading}
+              </ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>{mBody}</ModalBody>
+              <ModalFooter>
+                <Button
+                  onClick={() => {
+                    onClose();
+                    setMBody(<CreateOptions />);
+                    setMHeading("Create");
+                  }}
+                >
+                  Close
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+          {/* {Image Header} */}
+          <ImageHeader header={profHeader} avatar={profAvatar} />
           <HStack
+            fontFamily={"NexaBold"}
+            paddingTop={"4.5rem"}
             alignItems={"center"}
-            justifyContent={"flex-start"}
-            gap={"0.69rem"}
+            gap={["1.5rem", "1.5rem", "3.5rem"]}
           >
-            <Text fontSize={["1.25rem", "1.25rem", "2.25rem"]} fontWeight={400}>
-              {wallet?.accountId?.slice(
-                0,
-                wallet?.accountId?.split(".")[0].length
-              )}
-            </Text>
-            <GreyVerified />
-          </HStack>
-          <HStack>
+            <HStack
+              alignItems={"center"}
+              justifyContent={"flex-start"}
+              gap={"0.69rem"}
+            >
+              <Text
+                fontSize={["1.25rem", "1.25rem", "2.25rem"]}
+                fontWeight={400}
+              >
+                {wallet?.accountId?.slice(
+                  0,
+                  wallet?.accountId?.split(".")[0].length
+                )}
+              </Text>
+              <GreyVerified />
+            </HStack>
+            <HStack>
+              <InfoText
+                fontSize={["1rem", "1rem", "1.5rem"]}
+                fontWeight={400}
+                gtext="WALLET ADDRESS"
+                wtext={wallet?.accountId}
+              />
+              <Clipboard handleCopy={onCopy} />
+            </HStack>
             <InfoText
               fontSize={["1rem", "1rem", "1.5rem"]}
               fontWeight={400}
-              gtext="WALLET ADDRESS"
-              wtext={wallet?.accountId}
+              gtext="JOINED"
+              wtext="Oct 2023"
             />
-            <Clipboard handleCopy={onCopy} />
           </HStack>
-          <InfoText
-            fontSize={["1rem", "1rem", "1.5rem"]}
-            fontWeight={400}
-            gtext="JOINED"
-            wtext="Oct 2021"
-          />
-        </HStack>
-        {/* {User Info} */}
-        {/* <Text
+          {/* {User Info} */}
+          {/* <Text
           fontFamily={"NexaBold"}
           color={"#B3B3B3"}
           fontSize={["1rem", "1rem", "1.5rem"]}
@@ -209,86 +228,86 @@ function Profile() {
           experiences and activations paid for by the Doodles Community
           Treasury.
         </Text> */}
-        {/* {User Stats} */}
-        <HStack
-          alignItems={"center"}
-          justifyContent={"space-between"}
-          marginY={"3rem"}
-        >
-          <InfoText
-            fontSize={["0.7rem", "0.7rem", "1.5rem"]}
-            fontWeight={400}
-            gtext="COLLECTION VALUE"
-            wtext="289,099 NEAR"
-            wdec={true}
-          />
-          <InfoText
-            fontSize={["0.7rem", "0.7rem", "1.5rem"]}
-            fontWeight={400}
-            gtext="NO OF COLLECTION"
-            wtext={`${userCollections?.length || 0}`}
-            wdec={true}
-          />
-        </HStack>
-        {/* {User Collections} */}
-        <HStack
-          fontFamily={"NexaBold"}
-          alignItems={"center"}
-          justifyContent={"space-between"}
-          flexWrap={"wrap"}
-        >
-          <Select
-            borderRadius={"1rem"}
-            borderColor={"#B3B3B3"}
-            color={"#B3B3B3"}
-            fontSize={["0.8rem", "0.8rem", "1rem"]}
-            w={["100vw", "100vw", "19.75rem"]}
-            placeholder="Recently Added"
-          ></Select>
-          <InputGroup
-            borderRadius={"1rem"}
-            w={["100vw", "100vw", "34.25rem"]}
-            borderColor={"#B3B3B3"}
-            display={"flex"}
+          {/* {User Stats} */}
+          <HStack
             alignItems={"center"}
-            justifyContent={"flex-start"}
-            border={"1px solid"}
-            padding={["0.3rem", "0.3rem", "0.625rem"]}
-            fontSize={["0.8rem", "0.8rem", "1rem"]}
+            justifyContent={"space-between"}
+            marginY={"3rem"}
           >
-            <SearchIcon />
-            <Input
-              border={"none"}
-              color={"#B3B3B3"}
-              type="text"
-              placeholder="Search"
+            <InfoText
+              fontSize={["0.7rem", "0.7rem", "1.5rem"]}
+              fontWeight={400}
+              gtext="COLLECTION VALUE"
+              wtext={`${volume || "-"} NEAR`}
+              wdec={true}
             />
-          </InputGroup>
-          <Button variant={"solid"} colorScheme="green" onClick={onOpen}>
-            <Text
+            <InfoText
+              fontSize={["0.7rem", "0.7rem", "1.5rem"]}
+              fontWeight={400}
+              gtext="NO OF COLLECTION"
+              wtext={`${userCollections?.length || 0}`}
+              wdec={true}
+            />
+          </HStack>
+          {/* {User Collections} */}
+          <HStack
+            fontFamily={"NexaBold"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            flexWrap={"wrap"}
+          >
+            <Select
+              borderRadius={"1rem"}
+              borderColor={"#B3B3B3"}
+              color={"#B3B3B3"}
+              fontSize={["0.8rem", "0.8rem", "1rem"]}
+              w={["100vw", "100vw", "19.75rem"]}
+              placeholder="Recently Added"
+            ></Select>
+            <InputGroup
+              borderRadius={"1rem"}
+              w={["100vw", "100vw", "34.25rem"]}
+              borderColor={"#B3B3B3"}
+              display={"flex"}
+              alignItems={"center"}
+              justifyContent={"flex-start"}
+              border={"1px solid"}
               padding={["0.3rem", "0.3rem", "0.625rem"]}
               fontSize={["0.8rem", "0.8rem", "1rem"]}
             >
-              &#43; Create
-            </Text>
-          </Button>
-        </HStack>
-        {/* {User NFT Cards} */}
-        <HStack
-          marginY={"3rem"}
-          alignItems={"center"}
-          gap={"0.94rem"}
-          flexWrap={"wrap"}
-        >
-          {userTokens?.map((nft: any) => (
-            <NftCard key={nft.token_id} nft={nft} />
-          ))}
-        </HStack>
-      </Stack>
-      {/* {Footer} */}
-      <Footer />
-    </main>
-  );
+              <SearchIcon />
+              <Input
+                border={"none"}
+                color={"#B3B3B3"}
+                type="text"
+                placeholder="Search"
+              />
+            </InputGroup>
+            <Button variant={"solid"} colorScheme="green" onClick={onOpen}>
+              <Text
+                padding={["0.3rem", "0.3rem", "0.625rem"]}
+                fontSize={["0.8rem", "0.8rem", "1rem"]}
+              >
+                &#43; Create
+              </Text>
+            </Button>
+          </HStack>
+          {/* {User NFT Cards} */}
+          <HStack
+            marginY={"3rem"}
+            alignItems={"center"}
+            gap={"0.94rem"}
+            flexWrap={"wrap"}
+          >
+            {userTokens?.map((nft: any) => (
+              <NftCard key={nft.token_id} nft={nft} />
+            ))}
+          </HStack>
+        </Stack>
+        {/* {Footer} */}
+        <Footer />
+      </main>
+    );
+  }
 }
-
 export default Profile;
